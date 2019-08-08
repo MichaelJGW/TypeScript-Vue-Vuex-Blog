3f9fb513ed9235381a9943caa5e8415cdd688a83
Michael Warner <MichaelWarner@EuropaSports.com>
2019-08-08 10:55:55 -0400

    update

---
+++ src/store/about.ts
@@ -0,0 +1,30 @@
+import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators';
+import Store from '../store';
+
+@Module({
+    dynamic: true,
+    store: Store,
+    name: 'about',
+    namespaced: true,
+})
+export default class UserAdminModule extends VuexModule {
+
+    public firstName: string = 'bob';
+    public lastName: string = 'smith';
+
+    public get fullName(): string {
+        return this.firstName + ' ' + this.lastName;
+    }
+
+    @Mutation
+    public setNames(payload: {name: string}) {
+        const names: string[] = payload.name.split(' ');
+        this.firstName = names[0];
+        this.lastName = names[1];
+    }
+
+    @Action({ commit: 'setNames'})
+    public async setNamesAsync(payload: {names: string}) {
+        return await payload.names;
+    }
+}
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,36 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div>
+      title = {{title}}
+    </div>
+    <div>
+      fullName = {{aboutState.fullName}}
+    </div>
+    <div>
+      <!-- getterNum = {{getterNum}} -->
+    </div>
+    <button @click="changeName()">Change State</button>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Emit } from 'vue-property-decorator';
+import { State, Getter, Action, Mutation, namespace } from 'vuex-class';
+import { getModule } from 'vuex-module-decorators';
+import about from '../store/about';
+
+@Component
+export default class HelloWorld extends Vue {
+  private num = 0 as number;
+  private title = 'Hello There' as string;
+  private aboutState = getModule(about) as about;
+
+  @Prop() private msg!: string;
+
+  private changeName(): void {
+    this.aboutState.setNames({name: 'hello there'});
+  }
+
+}
+</script>
